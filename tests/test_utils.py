80dff046db52412b3b1ca3bd9e61457bd81431fe
Simone Bendazzoli <simben@kth.se>
2025-10-22 09:30:18 +0000

    Add unit tests for get_available_models function in test_utils.py
    Introduced a new test file, test_utils.py, to validate the behavior of the get_available_models function. The tests include setup for mock authentication and simulate API responses, ensuring comprehensive coverage of model retrieval scenarios.

---
+++ tests/test_utils.py
@@ -0,0 +1,38 @@
+import unittest
+import tempfile
+import os
+from MONet.utils import get_available_models
+from unittest.mock import MagicMock, patch
+import json
+import time
+import shutil
+
+class TestMONetUtilsHelpers(unittest.TestCase):
+    
+    
+    def setUp(self):
+        self.username = "test@maia.se"
+        self.password = "my-secret-password!"
+        self.token = "test-token"
+        self.refresh_token = "test-refresh-token"
+        
+        self.temp_home_dir = tempfile.mkdtemp()
+        os.environ["HOME"] = self.temp_home_dir
+        os.makedirs(os.path.join(self.temp_home_dir, ".monet"), exist_ok=True)
+        
+        with open(os.path.join(self.temp_home_dir, ".monet", f"{self.username}_auth.json"), "w") as f:
+            json.dump({"access_token": self.token, "refresh_token": self.refresh_token}, f)
+            
+    def tearDown(self):
+        shutil.rmtree(self.temp_home_dir)
+        os.environ.pop("HOME")
+
+    @patch("MONet.utils.requests.post")
+    def test_get_available_models(self, mock_post):
+        mock_post.return_value = MagicMock(status_code=200, json=lambda: {"models": {"test-model-segmentation": {"monai_label": "https://test-model-segmentation-portal.com"}}})
+        models = get_available_models(token=self.token, username=self.username)
+        self.assertEqual(models, {"test-model": "https://test-model-segmentation-portal.com"})
+        mock_post.assert_called_once_with(
+            "https://maia.app.cloud.cbh.kth.se/maia/maia-segmentation-portal/models/",
+            data={"id_token": self.token, "username": self.username}
+        )
